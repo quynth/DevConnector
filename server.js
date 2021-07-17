8e73a0d4afed87a69a89809954aca0c190c999d3
Quynh <71189807+quynth@users.noreply.github.com>
2021-07-17 13:23:12 -0500

    Register api

--- server.js
+++ server.js
@@ -1,26 +1,29 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const keys = require('./config/keys');
-const users = require('./routes/api/users');
-const profile = require('./routes/api/profile');
-const posts = require('./routes/api/posts');
+const express = require("express");
+const mongoose = require("mongoose");
+const keys = require("./config/keys");
+const users = require("./routes/api/users");
+const profile = require("./routes/api/profile");
+const posts = require("./routes/api/posts");
 const app = express();
+
+//Body parser config
+app.use(express.urlencoded);
+app.use(express.json());
 
 //Db config
 const db = keys.mongoURI;
 mongoose
-    .connect(db)
-    .then(() => console.log('MongoDb connected'))
-    .catch(err => console.log ('err'))
-
+  .connect(db)
+  .then(() => console.log("MongoDb connected"))
+  .catch((err) => console.log("err"));
 
 // Let's write our first route
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get("/", (req, res) => res.send("Hello World!"));
 
 //Use routes
-app.use('/api/users', users);
-app.use('/api/profile', profile);
-app.use('/api/posts', posts);
+app.use("/api/users", users);
+app.use("/api/profile", profile);
+app.use("/api/posts", posts);
 
 const port = 5000;
-app.listen (port, () => console.log(`Server is running on port ${port}`));
+app.listen(port, () => console.log(`Server is running on port ${port}`));
