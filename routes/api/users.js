8280d8535bdebd7a0a73f3f56609df64a213091d
Quynh <71189807+quynth@users.noreply.github.com>
2021-07-27 19:59:18 -0700

    Completed API code

--- routes/api/users.js
+++ routes/api/users.js
@@ -7,6 +7,7 @@ const router = express.Router();
 const User = require('../../models/User');
 const keys = require('../../config/keys');
 const validateRegisterInput = require('../../validation/register');
+const validateLoginInput = require('../../validation/login');
 
 //@route   POST  api/users/register
 //@desc    Register a user
@@ -58,6 +59,13 @@ router.post('/register', (req, res) => {
 //@access  Public
 
 router.post('/login', (req, res) => {
+  //validation
+  const { errors, isValid } = validateLoginInput(req.body);
+
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
   User.findOne({ email: req.body.email })
     .then((user) => {
       if (!user) {
