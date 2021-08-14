9f8f3c581373f0e43f4ea008a1ef6bbc0fc3f4ba
Quynh <71189807+quynth@users.noreply.github.com>
2021-08-14 12:56:50 -0700

    added authreducer and action

--- client/src/store.js
+++ client/src/store.js
@@ -1,5 +1,15 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers';
 
-const store = createStore([], {}, applyMiddleware());
+const middleware = [thunk];
+const store = createStore(
+  rootReducer,
+  {},
+  compose(
+    applyMiddleware(...middleware),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+);
 
 export default store;
