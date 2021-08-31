8ac20709b0d8dd04375763e085db16f63eb2295c
Quynh <71189807+quynth@users.noreply.github.com>
2021-08-30 21:41:37 -0700

    Prepared for deploy

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const passport = require("passport");
-const keys = require("./config/keys");
-const users = require("./routes/api/users");
-const profile = require("./routes/api/profile");
-const posts = require("./routes/api/posts");
+const express = require('express');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const keys = require('./config/keys');
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
 const app = express();
 
 //Body parser config
@@ -15,20 +15,24 @@ app.use(express.json());
 const db = keys.mongoURI;
 mongoose
   .connect(db)
-  .then(() => console.log("MongoDb connected"))
-  .catch((err) => console.log("err"));
+  .then(() => console.log('MongoDb connected'))
+  .catch((err) => console.log('err'));
 
 //Passport config
 app.use(passport.initialize());
-require("./config/passport")(passport);
-
-//Let's write our first route
-app.get("/", (req, res) => res.send("Hello World!"));
+require('./config/passport')(passport);
 
 //Use routes
-app.use("/api/users", users);
-app.use("/api/profile", profile);
-app.use("/api/posts", posts);
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
 
-const port = 5000;
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
